04e7c05eff437b723bf1dc1a18f10f777fa203d1
Aarson Raut <aarson9099@gmail.com>
2026-02-18 08:12:22 +0000

    Docker and Kubernetes

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>🚀 Final Project</h1>
+      <h3>Vite + Jenkins + Docker + KIND Kubernetes</h3>
+
+      <button onClick={() => setCount(count + 1)}>
+        Clicked {count} times
+      </button>
+
+      <p style={{ marginTop: "20px", color: "green" }}>
+        Auto deployed using Jenkins CI/CD pipeline
+      </p>
+    </div>
+  )
+}
+
+export default App
